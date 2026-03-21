cab81d3ef33928d2629c036989a05f5fb8bd5374
giitto <giitto92600@gmail.com>
2026-03-21 16:28:02 +0100

    Editing capability
    Added possibility to edit each achievement element

--- Achievement/achievement_desc.gd
+++ Achievement/achievement_desc.gd
@@ -1,2 +1,58 @@
 extends VBoxContainer
 class_name Achievement
+
+@onready var icon: TextureRect = $AchievementsDesc/Icon
+@onready var icon_click: Button = $AchievementsDesc/Icon/IconClick
+@onready var image_selection: FileDialog = $AchievementsDesc/ImageSelection
+
+@onready var title: LineEdit = $AchievementsDesc/Title/Title
+@onready var desciption_text_edit: TextEdit = $AchievementsDesc/Title/DesciptionTextEdit
+@onready var guide_detail: RichTextLabel = $GuideDetail
+@onready var guide_edit: TextEdit = $GuideEdit
+var bbcode_string: String = ""
+
+
+func _ready() -> void:
+	icon_click.size = icon.size
+	bbcode_string = guide_detail.text
+	guide_edit.text = guide_detail.text
+	
+	## Setting up default state of FileDialog
+	image_selection.filters = PackedStringArray([
+	"*.bmp,*.dds,*.ktx,*.exr,*.hdr,*.jpg,*.jpeg,*.png,*.tga,*.svg,*.webp ; Image Files"
+	])
+	image_selection.set_file_mode(FileDialog.FILE_MODE_OPEN_FILE)
+	image_selection.set_access(FileDialog.ACCESS_FILESYSTEM)
+	image_selection.set_use_native_dialog(true) ## Made native
+	image_selection.set_force_native(true) ## Force native
+
+func toggle_edit(act : bool) -> void:
+	toggle_icon_onclick_event(act)
+	toggle_guide_text(act)
+	title.editable = act
+	desciption_text_edit.editable = act
+
+func toggle_guide_text(act : bool) -> void:
+	if act :
+		guide_edit.text = bbcode_string
+	else :
+		bbcode_string = guide_edit.text
+		guide_detail.clear()
+		guide_detail.append_text(guide_edit.text)
+	guide_detail.visible = !act
+	guide_edit.visible = act
+	guide_edit.editable = act
+	
+
+
+func toggle_icon_onclick_event(act : bool) -> void:
+	if act:
+		icon_click.pressed.connect(_icon_on_click_event)
+	else:
+		icon_click.pressed.disconnect(_icon_on_click_event)
+
+func _icon_on_click_event() -> void: 
+	image_selection.popup_centered_ratio()
+
+func _on_image_selection_file_selected(path: String) -> void:
+	icon.texture = ImageTexture.create_from_image(Image.load_from_file(path))
--- main.gd
+++ main.gd
@@ -2,24 +2,34 @@ extends VBoxContainer
 
 @onready var main: VBoxContainer = $"."
 @onready var menu_button: MenuButton = $VBoxContainer/MenuButton
+@onready var achievment_box: VBoxContainer = $ScrollContainer/AchievmentBox
 
-var troph_list:Array[Node] = [];
+var is_edit_on :bool = false
+
+var troph_list:Array[Achievement] = [];
 
 func _ready() -> void:
 	menu_button.get_popup().id_pressed.connect(_on_option_item_selected)
 	for ach in troph_list:
+		ach.toggle_edit(is_edit_on)
 		add_child(ach.instantiate())
 
 
 func _on_add_achivement_button_pressed() -> void:
 	var ui_scene = preload("res://Achievement/achievement_desc.tscn")
-	var ui_instance = ui_scene.instantiate()
-	add_child(ui_instance)
+	var ui_instance : Achievement = ui_scene.instantiate()
+	#ui_instance.toggle_edit(is_edit_on)
+	achievment_box.add_child(ui_instance)
 	troph_list.append(ui_instance)
-	print(troph_list)
 
 func _on_option_item_selected(index: int) -> void:
 	if index == 0:
 		print("Save") 
 	elif index == 1:
 		print("Load")
+
+
+func _on_edit_mode_toggled(toggled_on: bool) -> void:
+	is_edit_on = toggled_on
+	for troph : Achievement in troph_list:
+		troph.toggle_edit(toggled_on)
